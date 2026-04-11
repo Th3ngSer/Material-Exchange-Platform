a47d39edf03898db5278c2369d2c27d47155154b
Thongking <thongking1280@gmail.com>
2026-04-11 18:10:01 +0700

    First step with database X docker

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const databaseImports =
+  process.env.NODE_ENV === 'test'
+    ? []
+    : [
+      MongooseModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          uri: configService.get<string>(
+            'MONGODB_URI',
+            'mongodb://localhost:27017/material_xchange?replicaSet=rs0',
+          ),
+        }),
+      }),
+    ];
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ...databaseImports,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
