8b4c83772f008a3532b95927ab58f81999725bbd
PanhaSR <panhasreng96@gmail.com>
2026-04-23 08:35:22 +0700

    Add: dropdown Component

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import DropDownMenu from './components/DropDownMenu.vue';
+
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -6,6 +9,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <DropDownMenu/>ls
 </template>
 
 <style scoped></style>
