dbf6f2bd133a6ce0f6aa64483c4be6a17e366e96
Thongking <thongking1280@gmail.com>
2026-05-11 00:35:54 +0700

    Push to main

--- Frontend/src/router/index.ts
+++ Frontend/src/router/index.ts
@@ -13,11 +13,5 @@ export const router = createRouter({
   history: createWebHistory(),
   routes
 })
-import { createRouter, createWebHistory } from 'vue-router'
-
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
 
 export default router
